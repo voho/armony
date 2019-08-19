7dd26b15130222c4470784ace33f679982821272
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2019-08-19 23:44:56 +0200

    better placement of play button

--- src/components/KeyedScaleChart.tsx
+++ src/components/KeyedScaleChart.tsx
@@ -10,19 +10,21 @@ export interface KeyedScaleChartProps {
 export const KeyedScaleChart: React.FC<KeyedScaleChartProps> = (props) => {
     return (
         <div className="card mb-3">
-            <div className="card-header">
-                <h6>{getPitch(props.scale.getRoot())} <span className="badge badge-info">{props.scale.getScale().getName()}</span></h6>
+            <div className="card-header row">
+                <div className="col-auto">
+                    <h6>{getPitch(props.scale.getRoot())} <span className="badge badge-info">{props.scale.getScale().getName()}</span></h6>
+                </div>
+                <div className="col text-right">
+                    <button className="btn btn btn-outline-secondary btn-sm" onClick={() => play(props.scale)}>Play</button>
+                </div>
             </div>
             <div className="card-body">
-                {props.scale.generate().map((pitch) => {
-                    const classes = isBlack(pitch) ? "badge badge-dark mr-1" : "badge badge-light mr-1";
-                    return (<span className={classes}>{getPitch(pitch)}</span>);
-                })}
-            </div>
-            <div className="card-footer">
-                <a href="#" onClick={() => play(props.scale)}>
-                    <small>Play</small>
-                </a>
+                <p className="card-text">
+                    {props.scale.generate().map((pitch) => {
+                        const classes = isBlack(pitch) ? "badge badge-dark mr-1" : "badge badge-light mr-1";
+                        return (<span className={classes}>{getPitch(pitch)}</span>);
+                    })}
+                </p>
             </div>
         </div>
     );
